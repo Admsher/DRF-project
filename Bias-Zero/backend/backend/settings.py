2f6666f928f57998332b545c7451a372f962b63b
Varad Shinde <Varadshinde60@gmail.com>
2024-07-17 20:14:27 +0530

    LC

--- Bias-Zero/backend/backend/settings.py
+++ Bias-Zero/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^5f@aq%4^4ikhc3a@4@5rg*_9k4%!8ej^crna@=y&e(65^_b%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
