870703e78ac5f50a11e06aca8820d313de59be4e
Varad Shinde <Varadshinde60@gmail.com>
2024-07-19 17:56:52 +0530

    LC

--- Bias-Zero/backend/backend/settings.py
+++ Bias-Zero/backend/backend/settings.py
@@ -150,6 +150,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'username@gmail.com'
-EMAIL_HOST_PASSWORD = 'pass'
+EMAIL_HOST_USER = 'jitsi5043@gmail.com'
+EMAIL_HOST_PASSWORD = 'drke pycn wold bckc'
 
